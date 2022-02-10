5086c9197d3b0862c57b51cf7954dc7237b852bd
Kevin Fields <kfields31@gmail.com>
2022-02-10 11:30:49 -0600

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Label, Modal, ModalHeader, ModalBody, Button, Row, Col} from 'reactstrap';
 import {Control, LocalForm, Errors} from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 import {Link} from 'react-router-dom';
 
 const required = (val) => val && val.length;
@@ -8,7 +9,7 @@ const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
 
     function RenderDish({dish}){
-        if (dish && dish !== null) {
+        if (dish && dish != null) {
         return(
         <div className='col-12 col-md-5 m-1'>
         <Card>
@@ -107,7 +108,6 @@ const minLength = (len) => (val) => val && val.length >= len;
                                     minLength: minLength(2),
                                     maxLength: maxLength(15),
                                 }}/>
-                                </Col>
                                 <Errors 
                                         className='text-danger'
                                         model='.author'
@@ -118,6 +118,7 @@ const minLength = (len) => (val) => val && val.length >= len;
                                             maxLength: 'Must be 15 characters or less'
                                         }}
                                        /> 
+                                </Col>
                             </Row>
                             <Row className="form-group">
                                 <Col>
@@ -141,30 +142,47 @@ const minLength = (len) => (val) => val && val.length >= len;
 
 
     const DishDetail = (props) => {
-    if (props.dish) {
-        return (
-            <div className="container">
-            <div className="row">
-                <Breadcrumb>
+        
+        if (props.isLoading) {
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        <Loading />
+                    </div>
+                </div>
+            )
+        } else if (props.errMess) {
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            )
+        } else if (props.dish != null) {
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
 
-                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                </div>                
-            </div>
-            <div className="row">
-                <RenderDish dish={props.dish} />
-                <RenderComments comments={props.comments}
-                    addComment={props.addComment}
-                    dishId={props.dish.id} />
-            </div>
-            </div>
-        );
-    } else {
-        return <div></div>;
-    }
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                    </div>                
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
+                </div>
+                </div>
+            );
+        } else {
+            return <div></div>;
+        }
     }
 
 
