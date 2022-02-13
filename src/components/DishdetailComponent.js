d4d5724b8f5d3aebb1233d720f1d739e4e819dc9
Kevin Fields <kfields31@gmail.com>
2022-02-13 13:13:01 -0600

    Fetch postComments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ const minLength = (len) => (val) => val && val.length >= len;
         }
     }
 
-    function RenderComments({comments, addComment, dishId}){
+    function RenderComments({comments, postComment, dishId}){
         if (comments && comments !== null) {
         return (
             <div className='col-12 col-md-5 m-1'>
@@ -41,7 +41,7 @@ const minLength = (len) => (val) => val && val.length >= len;
             ))}
             </ul>
             <CommentForm dishId={dishId}
-                addComment={addComment}
+                postComment={postComment}
                 />
             </div>
         );
@@ -72,7 +72,7 @@ const minLength = (len) => (val) => val && val.length >= len;
 
         handleSubmit(values) {
             this.toggleModal();
-            this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+            this.props.postComment(this.props.dishId, values.rating, values.comment, values.author);
         }
 
         render() {
@@ -176,7 +176,7 @@ const minLength = (len) => (val) => val && val.length >= len;
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         dishId={props.dish.id} />
                 </div>
                 </div>
